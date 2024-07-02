96896e7a80c5ae71f3fd86081383acf322708add
jungminji <wjdalswl0215@gmail.com>
2024-07-03 03:50:40 +0900

    버튼 누르면 모달창 뜨게 해보기

---
+++ codingapple/modal/main.js
@@ -0,0 +1,8 @@
+document
+  .getElementById("click-modal")
+  .addEventListener("click", () => showModal());
+
+function showModal() {
+  document.getElementsByClassName("black-bg")[0].style.display = "block";
+  console.log("sfds");
+}
