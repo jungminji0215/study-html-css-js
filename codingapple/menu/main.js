106bf1431df83b9303ccc4e3114db553274344dd
jungminji <wjdalswl0215@gmail.com>
2024-07-17 17:46:28 +0900

    div 끝까지 스크롤하면 alert 띄우기

--- codingapple/menu/main.js
+++ codingapple/menu/main.js
@@ -15,3 +15,18 @@ window.addEventListener("scroll", function () {
   document.querySelector(".navbar-brand").style.fontSize =
     position > 100 ? "30px" : "50px";
 });
+
+const scrollAlertDiv = document.querySelector(".scroll-alert");
+
+// 스크롤 이벤트 리스너를 추가합니다.
+scrollAlertDiv.addEventListener("scroll", function () {
+  // 스크롤 위치와 요소의 높이를 가져옵니다.
+  const scrollTop = scrollAlertDiv.scrollTop;
+  const scrollHeight = scrollAlertDiv.scrollHeight;
+  const clientHeight = scrollAlertDiv.clientHeight;
+
+  // 요소가 맨 아래에 도달했는지 확인합니다.
+  if (scrollTop + clientHeight >= scrollHeight) {
+    alert("글을 다 읽으셨군요!");
+  }
+});
